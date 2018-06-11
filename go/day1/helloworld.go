9d1de5b60ff8bf240d4adb6d2792e718f574b910
pple <sanzhixiong1986@sina.cn>
2018-06-12 00:06:36 +0700

    提交一个go文件

--- go/day1/helloworld.go
+++ go/day1/helloworld.go
@@ -1,9 +1,11 @@
-package main
+package main //和java一样就是是包路径也就是文件夹路径
 
 import (
-	"fmt"
+	"fmt" //类似于c语言的#include java的import 一样
 )
 
+//func 函数 主函数
 func main() {
+	//打印函数
 	fmt.Println("helloword")
 }
